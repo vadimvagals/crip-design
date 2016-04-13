b1389d602ff52b86a2e3698af123f35a97e2565f
Vadims <vadjan07@inbox.lv>
2016-04-13 22:21:16 +0300

    new

---
+++ crip_design/gulpfile.js
@@ -0,0 +1,22 @@
+﻿/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+
+
+
+var gulp = require('gulp'),
+ webhelp = require('webhelp');
+
+webhelp.sass('./assets/sass/style.scss', './assets/sass/**/*.scss', 'styles', false, './assets/css');
+webhelp.copy('./bower_components/jquery/dist/jquery.min.js', './assets/js', 'copy_jq');
+webhelp.copy('./bower_components/particles.js/particles.min.js', './assets/js', 'copy_particles');
+//
+webhelp.copy('./bower_components/bootstrap-sass/assets/javascripts/bootstrap.min.js', './assets/js', 'copy_bootstrap_js');
+webhelp.scripts('./scripts/app/**/*.js', 'app', 'concat_js', '', './assets/js');
+
+gulp.task('default', function () {
+    //webhelp.gulp.start('webhelp-default'); // run all regitered task
+    webhelp.watch();
+});
---
+++ crip_design/scripts/app/app.js
@@ -0,0 +1,11 @@
+﻿
+(function ($, document) {
+
+    $(document).ready(function () {
+        $(".navbar-toggle").on("click", function () {
+            $(this).toggleClass("active");
+        });
+    });
+
+})(jQuery, document);
+
